30f90358bad28975e5d44fd739b38502ea09de19
xuxufei <xuxufei@2144.cn>
2018-10-11 16:10:37 +0800

    compatibility ie7

---
+++ lib/ultimate-pagination-utils.d.ts
@@ -0,0 +1,2 @@
+export declare function createRange(start: number, end: number): number[];
+export declare function map(arr: any[], callbackfn: (value: any, index: number, array: any[]) => {}, thisArg?: any): any[];
